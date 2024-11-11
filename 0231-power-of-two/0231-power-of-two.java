313cbe85d8948faee509160bcd0314f4a5560150
skshm11 <55909498+skshm11@users.noreply.github.com>
2024-11-11 13:04:53 +0000

    Time: 1 ms (47.77%), Space: 40.9 MB (39.68%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 0) return false;
+        return helper(n);
+    }
+
+    public boolean helper(int n){
+        if(n == 1) return true;
+
+        if(n % 2 == 0){
+            return helper(n/2);
+        }else{
+            return false;
+        }
+
+    }
+}
