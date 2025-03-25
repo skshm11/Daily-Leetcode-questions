1ada005528bae232e1acee28c3ef2c2c6116763e
SakshamNasa <55909498+skshm11@users.noreply.github.com>
2025-03-25 02:03:42 +0000

    Time: 62 ms (43.07%), Space: 61.7 MB (24.6%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -10,4 +10,10 @@ class Solution {
 
         return pq.peek();
     }
+
+// Notes:
+//Each element gets inserted once → n × O(log k)
+//Each insertion (offer) into the heap takes O(log m) time, where m is current heap size
+//Each removal (poll) from the heap also takes O(log m) time
+//Since we maintain the heap size ≤ k, all operations are O(log k)    
 }
