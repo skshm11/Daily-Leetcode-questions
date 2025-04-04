c1e25395dc6cbe1239630973f41559dc8c87ecf3
SakshamNasa <55909498+skshm11@users.noreply.github.com>
2025-04-04 03:48:36 +0100

    Time: 0 ms (100%), Space: 41.6 MB (99.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,34 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       ArrayList<Integer> al = new ArrayList<>();
-        int i =0;
-        int j =0;
-         while(i<m && j<n){
-        if(nums1[i] <= nums2[j]){
-            al.add(nums1[i]);
-            i++;
-        }else //if (nums2[j]<nums1[i])
-        {
-            al.add(nums2[j]);
-            j++;
-        }
-         }
-             
-         while(i<m){    
-        //else{
-            //if(i>=m){
-                al.add(nums1[i]);
-                i++;
-            }
-            //else{
-             while(j<n){
-                al.add(nums2[j]);
-                j++;
-            }
+        int i = m - 1, j = n - 1, k = m+n-1;
         
-         
-        for(int k=0; k<al.size(); k++  ){
-            nums1[k]= al.get(k);
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i];
+                i--;
+            }else{
+                nums1[k--] = nums2[j];
+                j--;
+            }
+        }
+
+        while(j>=0){
+            nums1[k--] = nums2[j];
+            j--;
         }
     }
 }
