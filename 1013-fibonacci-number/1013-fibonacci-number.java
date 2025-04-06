8332f28ad6435e6356421bf7d864118b5b766457
SakshamNasa <55909498+skshm11@users.noreply.github.com>
2025-04-06 04:13:35 +0100

    Time: 9 ms (24.2%), Space: 40.4 MB (48.6%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,8 +1,11 @@
 class Solution {
     public int fib(int n) {
-        if(n==1) return 1;
-        if(n==0) return 0;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
 
-        return fib(n-1)+fib(n-2);
+        return fib(n-1) + fib(n-2);
     }
+
+    // This is recursion. This can be optimised using DP to improve the space complexity!
+    // Memoisation, Tabulation, 2 pointers
 }
