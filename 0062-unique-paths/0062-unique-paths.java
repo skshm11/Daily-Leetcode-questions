366af6a067933739977aa0f3a2da7ce365c7024a
SakshamNasa <55909498+skshm11@users.noreply.github.com>
2025-04-08 14:08:20 +0100

    Time: 0 ms (100%), Space: 40.8 MB (26.48%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(m-1, n-1, dp);
+    }
+
+    private int helper(int m, int n, int[][] dp){
+        // base case
+        if(m == 0 && n == 0) return 1;
+        if(m < 0 || n < 0) return 0;
+
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int up = helper(m-1, n, dp);
+        int left = helper(m, n-1, dp);
+
+        return dp[m][n] = (up+left);
+    }
+}
