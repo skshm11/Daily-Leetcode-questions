a69bf40502dcb908d43145c49cb54ea025dc30cf
SakshamNasa <55909498+skshm11@users.noreply.github.com>
2025-04-13 14:09:04 +0100

    Time: 4 ms (93.19%), Space: 43.2 MB (51.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -8,7 +8,7 @@ class Solution {
         }
 
         if(sum-target < 0) return 0;
-        if((sum-target) % 2 != 0) return 0;
+        if((sum-target) % 2 != 0) return 0; // the bracket here is imp becaue it takes target% 2 rather than sum-target % 2
 
         int s2 = (sum-target)/2;
         int[][] dp = new int[n][s2+1];
@@ -20,10 +20,10 @@ class Solution {
     }
 
     private int helper(int i, int t, int[] nums, int[][] dp){
-        //base condition
+        //base condition -> rewatch dp 18 video- striver
         if(i == 0){
             if (t == 0 && nums[0] == 0)
-                return 2;
+                return 2; 
             // Check if the target is equal to the first element or 0
             if (t == 0 || t == nums[0])
                 return 1;
@@ -41,3 +41,5 @@ class Solution {
         return dp[i][t] = (taken + notTaken);
     }
 }
+// The code is exactly same as partition equal subset sum. All we do is convert the target given into s2 using equations.
+// why this is same as partition equal subset sum? Because now we are partioning it in such a way that all the values with + sign form 1 subset, and the values with - sign the other. Then we subtract them as mentioned in the description.
